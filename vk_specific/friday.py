9c84b5b3625b685d850228db1893bac88407126e
platofff <42435266+platofff@users.noreply.github.com>
2021-09-11 00:10:33 +0300

    Fix

--- vk_specific/friday.py
+++ vk_specific/friday.py
@@ -16,7 +16,7 @@ class Friday:
             date = datetime.fromtimestamp(timestamp)
             if date.weekday() == 4:
                 return 'До ПЯТНИЦЫ осталось ' +\
-                    str(datetime.fromtimestamp(datetime(date.year, date.month, date.day).timestamp()) - date)\
+                    str(datetime.fromtimestamp(datetime(date.year, date.month, date.day).timestamp()) - now)\
                     .replace('-1 day, ', '')[:-7].replace('1 day', '1 день').replace('2 days', '2 дня')\
                     .replace('3 days', '3 дня').replace('4 days', '4 дня').replace('days', 'дней') + '!'
 
