d56439ef79af17c597189b327ad99069f59bc54e
platofff <42435266+platofff@users.noreply.github.com>
2022-01-31 12:42:53 +0300

    [Testing] /cum

--- vk_specific/blueprints/cum.py
+++ vk_specific/blueprints/cum.py
@@ -12,9 +12,11 @@ bp = Blueprint()
 async def cum_handler(message: Message):
     photo = await utils.get_photo_url(message)
     if photo is None:
-        _, photos, _ = await utils.unpack_fwd(message, photos_max=1)
-        if not photos:
+        _, photos, _ = await utils.unpack_fwd(message)
+        photos = photos.values()
+        try:
+            photo = photos[0]
+        except IndexError:
             return await message.answer('Прикрепи или перешли изображение.')
-        photo = photos[0]
     fut = utils.pool.submit(utils.common.cum.overlay, photo)
     fut.add_done_callback(functools.partial(utils.photo_callback, message))
